d1b6cbb5ab039c97e7fd90db2cd5be1d59ce599d
Francisco <chico.81@hotmail.com>
2021-11-29 17:23:02 -0300

    feat: implement table and formulary components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
 
 export default function Home() {
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
 
   const clientes = [
     new Cliente('Ana', 34, '1'),
@@ -12,11 +17,21 @@ export default function Home() {
   ]
 
   function clienteSelecionado(cliente: Cliente) {
-    console.log(cliente.nome)
+    setCliente(cliente)
+    setVisivel('form')
   }
 
   function clienteExcluido(cliente: Cliente) {
     console.log('Excluir: ', cliente.nome)
+  }
+
+  function salvarCliente(cliente: Cliente) {
+    setVisivel('tabela')
+  }
+
+  function novoCliente() {
+    setCliente(Cliente.vazio())
+    setVisivel('form')
   }
 
   return (
@@ -26,10 +41,29 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <Tabela clientes={clientes}
-          clienteSelecionado={clienteSelecionado}
-          clienteExcluido={clienteExcluido}
-        ></Tabela>
+        {visivel === 'tabela' ? (
+          <>
+            <div className="flex justify-end">
+              <Botao
+                cor='green'
+                className="mb-4"
+                onClick={novoCliente}
+              >
+                Novo Cliente
+              </Botao>
+            </div>
+            <Tabela clientes={clientes}
+              clienteSelecionado={clienteSelecionado}
+              clienteExcluido={clienteExcluido}
+            />
+          </>
+        ) : (
+            <Formulario
+              cliente={cliente}
+              clienteMudou={salvarCliente}
+              cancelado={() => setVisivel('tabela')}
+            />
+        )}
       </Layout>
     </div>
   )
